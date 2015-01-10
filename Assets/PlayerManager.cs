3d3e97d68883e99f1583be368a1e2af54b7d3aa1
isotian <more.jeschke@googlemail.com>
2015-01-10 17:24:01 +0100

    UI

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public bool Deploying;
+	public float ChossenColor;
+	public Spaceship ChossenPattern;
+	public Planet ChossenPlanet;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ChossenColor != 0)
+						Deploying = true;
+		
+	}
+	
+	public void Desploying(){
+		Deploying = true;
+	}
+
+
+	public void Ship1Choosen(){
+		ChossenColor = 1;
+		}
+
+	public void Ship2Choosen(){
+		ChossenColor =  2;
+		}
+
+	public void Ship3Choosen(){
+		ChossenColor =  3;
+		}
+
+
+	public void ChooseShip(int colornumber){
+		ChossenColor = colornumber;
+
+		}
+
+
+
+
+
+
+
+
+}
