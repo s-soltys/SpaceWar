c64303c457d94026fc79e0a86273f41e00583a1c
Szymon Sołtysiak <simon@doctorkinetic.nl>
2015-04-12 19:27:58 +0200

    Make game faster.

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -5,6 +5,6 @@ public static class GameParameters
 {
     public static int GridSizeX = 10;
     public static int GridSizeY = 7;
-    public static float TickPeriod = 2.5f;
-    public static float TickMovementTime = 1.5f;
+    public static float TickPeriod = 1.5f;
+    public static float TickMovementTime = 1.0f;
 }
