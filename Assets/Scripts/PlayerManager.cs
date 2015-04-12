79fbf0afd2c4e536dbc5424c118999642b365caf
Szymon Sołtysiak <simon@doctorkinetic.nl>
2015-04-12 19:53:59 +0200

    Implement flick gesture for ship deployment.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,217 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
+    const float flickThreshold = 0.1f;
 
-	// it doesnt work well
-	public bool Deploying;
-	public SpaceshipColor ChoosenColor;
+    private Vector2 selectPosition;
 
+    public int startingGridPosition;
+    public Direction launchDirection;
+    public SpaceshipColor ChoosenColor;
+    public SpaceshipSpawn Spawn;
+    public float shootCooldown;
+    public float shootingRate = 1f;
 
-	// tags for player 1 and 2 to decide in the start function which ChoosenDirection should be choosen
-//	public Direction ChoosenDirection;
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
 
-	public string ChoosenPattern;
+    void Start()
+    {
+        shootCooldown = 0f;
+    }
 
-	
-	public SpaceshipSpawn Yes;
+    public void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
 
+    public void ChoosenBlue()
+    {
+        ChoosenColor = SpaceshipColor.Blue;
+    }
 
+    public void ChoosenRed()
+    {
+        ChoosenColor = SpaceshipColor.Red;
+    }
 
-	public float shootCooldown;
+    public void ChoosenYellow()
+    {
+        ChoosenColor = SpaceshipColor.Yellow;
+    }
 
-	public float shootingRate = 1f;
+    public void SelectPosition(BaseEventData eventData)
+    {
+        var e = eventData as PointerEventData;
+        selectPosition = e.position;
+    }
 
-	public bool CanAttack
-	{
-		get
-		{
-			return shootCooldown <= 0f;
-		}
-	}
+    public void Launch(BaseEventData eventData)
+    {
+        var e = eventData as PointerEventData;
 
-	void Start () {
-		
-		shootCooldown = 0f;
-	}
+        var totalDelta = (e.position - selectPosition) / Screen.width;
+        Debug.Log(Time.deltaTime + " " + totalDelta);
+        
+        var choosenPattern = string.Empty;
+        if (totalDelta.y > flickThreshold) choosenPattern = "DIAGONALUP";
+        else if (totalDelta.y < -flickThreshold) choosenPattern = "DIAGONALDOWN";
+        else if (Mathf.Abs(totalDelta.x) > flickThreshold) choosenPattern = "STRAIGHT";
+        else return;
 
-	public void Update (){
-		if (shootCooldown > 0)
-		{
-			shootCooldown -= Time.deltaTime;
-		}
+        var yPositionIndex = int.Parse(((GameObject)e.pointerDrag).name);
 
-		}
-
-
-
-
-	public void ChoosenBlue(){
-
-		ChoosenColor = SpaceshipColor.Blue;
-		Deploying = true;
-		}
-
-	public void ChoosenRed(){
-		
-		ChoosenColor = SpaceshipColor.Red;
-		Deploying = true;
-	}
-
-	public void ChoosenYellow(){
-
-		ChoosenColor = SpaceshipColor.Yellow;
-		Deploying = true;
-		}
-
-	
-
-
-
-	public void ChooseColor(){
-
-
-
-	}
-
-
-
-
-
-
-	public void ChooseStraight(){
-		
-		ChoosenPattern = "STRAIGHT";
-		
-	}
-
-	public void ChooseDiagonalUp(){
-		
-		ChoosenPattern = "DIAGONALUP";
-	
-	}
-	
-	public void ChooseDiagonalDown(){
-
-		ChoosenPattern = "DIAGONALDOWN";
-
-	}
-
-
-
-	public void Player1Planet1(){
-		if (CanAttack) {
-		shootCooldown = shootingRate;
-		Yes.Spawn (ChoosenColor,ChoosenPattern,Direction.Right, 0, 0);
-		Deploying = false;
-			}
-	}
-	public void Player1Planet2(){
-		if (CanAttack) {
-		shootCooldown = shootingRate;
-		Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 1);
-		Deploying = false;
-				}
-	}
-	public void Player1Planet3(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 2);
-						Deploying = false;
-				}
-	}
-	public void Player1Planet4(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 3);
-						Deploying = false;
-				}
-	}
-	public void Player1Planet5(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 4);
-						Deploying = false;
-				}
-	}
-	public void Player1Planet6(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 5);
-						Deploying = false;
-				}
-	}
-	public void Player1Planet7(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Right, 0, 6);
-						Deploying = false;
-				}
-	}
-
-
-
-
-
-
-
-
-
-
-
-	public void Player2Planet1(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 0);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet2(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 1);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet3(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 2);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet4(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 3);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet5(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 4);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet6(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 5);
-						Deploying = false;
-				}
-	}
-	public void Player2Planet7(){
-		if (CanAttack) {
-						shootCooldown = shootingRate;
-						Yes.Spawn (ChoosenColor, ChoosenPattern, Direction.Left, 9, 6);
-						Deploying = false;
-				}
-	}
-
-
-
-
-
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+            Spawn.Spawn(ChoosenColor, choosenPattern, launchDirection, startingGridPosition, yPositionIndex);
+        }
+    }
 
 }
