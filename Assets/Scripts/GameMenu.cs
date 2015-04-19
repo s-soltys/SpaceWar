444ebc7c965785f66704b874abb8072135a476da
Szymon Sołtysiak <simon@doctorkinetic.nl>
2015-04-19 20:34:48 +0200

    Add in-game menu.

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour
+{
+    public void StartSinglePlayer()
+    {
+        GameParameters.Mode = GameMode.Single;
+        Application.LoadLevel("Game");
+    }
+
+    public void StartSplitScreen()
+    {
+        GameParameters.Mode = GameMode.SplitScreen;
+        Application.LoadLevel("Game");
+    }
+
+}
--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public static class GameParameters
 {
+    public static GameMode Mode = GameMode.Single;
     public static int GridSizeX = 10;
     public static int GridSizeY = 7;
     public static float TickPeriod = 1.5f;
     public static float TickMovementTime = 1.0f;
+
+}
+
+public enum GameMode
+{
+    Single,
+    SplitScreen
 }
